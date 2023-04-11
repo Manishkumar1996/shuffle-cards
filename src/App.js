7c593f1bbeeca04640e59fd12d46aad3584df270
manishkumarprajapat <manish@fanclash.in>
2023-04-11 15:17:59 +0530

    Shuffle cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import {IMAGES} from "./constants";
+import {useCallback, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cardNames, setCardNames] = useState([]);
+    const [nextIndex, setNextIndex] = useState(0);
+    const [message, setMessage] = useState("");
+
+    const handleShuffle = useCallback(() => {
+        const shuffledNames = Object.values(IMAGES.cards);
+        for (let i = shuffledNames.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledNames[i], shuffledNames[j]] = [shuffledNames[j], shuffledNames[i]];
+        }
+        setCardNames(shuffledNames)
+        setNextIndex(0);
+        setMessage("Click for next card");
+    }, [])
+
+    const handleNext = () => {
+        console.error("handleNext")
+        if (!cardNames.length) {
+            return setMessage("Please shuffle first");
+        }
+        setNextIndex((prevState) => {
+            let value = prevState
+            if (cardNames[value + 1]) {
+                setMessage(`${value + 2} / 52`)
+                return value + 1
+            }
+            else setMessage("Cards Over, Please shuffle again or reset");
+
+            return value
+        })
+    }
+
+    const handleReset = () => {
+        setNextIndex(0);
+        setCardNames([]);
+        setMessage("")
+    }
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {!!message && <p className="message">{message}</p>}
+                <img src={cardNames[nextIndex] || IMAGES.backCard} className="App-logo" alt="logo"
+                    onClick={handleNext} />
+                <div className="mt-30">
+                    <button className="btn" onClick={handleShuffle}>Shuffle</button>
+                    <button className="btn ml-10" onClick={handleReset}>Reset</button>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
